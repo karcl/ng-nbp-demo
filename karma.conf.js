0c45058bee4c102d5e5c13f79e2868d6c2692125
Karol Chodakowski <flataux@gmail.com>
2015-11-15 16:17:54 +0100

    Much nicer spec reporter for karma

--- karma.conf.js
+++ karma.conf.js
@@ -3,6 +3,11 @@
 
 module.exports = function(config) {
   config.set({
+    plugins: [
+        'karma-jasmine',
+        'karma-phantomjs-launcher',
+        'karma-spec-reporter'
+    ],
 
     // base path that will be used to resolve all patterns (eg. files, exclude)
     basePath: '',
@@ -37,7 +42,7 @@ module.exports = function(config) {
     // test results reporter to use
     // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['progress'],
+    reporters: ['spec'],
 
 
     // web server port
